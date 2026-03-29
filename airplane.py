f3dd724929956701e6cb4bc3fa584a17bb50b83a
bigmacsssss <liamtxd14@gmail.com>
2026-03-29 14:46:49 -0400

    Implement movement
    Added controls for moving and shooting in the turtle graphics program.

--- airplane.py
+++ airplane.py
@@ -1,1 +1,32 @@
+import turtle, time
 
+t = turtle.Turtle()
+t.color("blue")
+t.speed(0)
+
+last_time = 0
+
+screen = turtle.Screen()
+screen.tracer(0)
+screen.listen()
+
+def move_forward():
+    t.forward(20)
+
+def move_backward():
+    t.backward(20)
+
+def turn_left():
+    t.left(15)
+
+def turn_right():
+    t.right(15)
+
+def shoot():
+    b=bullet()
+    bullets.append(b)
+running = True
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
